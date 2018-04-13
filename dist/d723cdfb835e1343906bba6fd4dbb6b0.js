4ad4ca062d10cc9d4f685cb03dcfb0301db618f8
Matthew Smith <matthewsmithwebdesign@gmail.com>
2018-04-13 08:44:40 -0700

    submit button working

--- dist/d723cdfb835e1343906bba6fd4dbb6b0.js
+++ dist/d723cdfb835e1343906bba6fd4dbb6b0.js
@@ -69,7 +69,7 @@ require = (function (modules, cache, entry) {
 
   // Override the current require with this new one
   return newRequire;
-})({8:[function(require,module,exports) {
+})({7:[function(require,module,exports) {
 var bundleURL = null;
 function getBundleURLCached() {
   if (!bundleURL) {
@@ -100,7 +100,7 @@ function getBaseURL(url) {
 exports.getBundleURL = getBundleURLCached;
 exports.getBaseURL = getBaseURL;
 
-},{}],7:[function(require,module,exports) {
+},{}],6:[function(require,module,exports) {
 var bundle = require('./bundle-url');
 
 function updateLink(link) {
@@ -132,17 +132,17 @@ function reloadCSS() {
 
 module.exports = reloadCSS;
 
-},{"./bundle-url":8}],5:[function(require,module,exports) {
+},{"./bundle-url":7}],3:[function(require,module,exports) {
 
         var reloadCSS = require('_css_loader');
         module.hot.dispose(reloadCSS);
         module.hot.accept(reloadCSS);
       
-},{"_css_loader":7}],3:[function(require,module,exports) {
+},{"_css_loader":6}],4:[function(require,module,exports) {
 "use strict";
 
 require("../scss/main.scss");
-},{"../scss/main.scss":5}],9:[function(require,module,exports) {
+},{"../scss/main.scss":3}],8:[function(require,module,exports) {
 
 var global = (1, eval)('this');
 var OldModule = module.bundle.Module;
@@ -162,7 +162,7 @@ module.bundle.Module = Module;
 
 if (!module.bundle.parent && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
-  var ws = new WebSocket('ws://' + hostname + ':' + '55415' + '/');
+  var ws = new WebSocket('ws://' + hostname + ':' + '57322' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
@@ -263,5 +263,5 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.require, id);
   });
 }
-},{}]},{},[9,3])
+},{}]},{},[8,4])
 //# sourceMappingURL=/dist/d723cdfb835e1343906bba6fd4dbb6b0.map
--- dist/fbb714a9d868a755d3f3ddba7c5cc70f.js
+++ dist/fbb714a9d868a755d3f3ddba7c5cc70f.js
@@ -69,7 +69,7 @@ require = (function (modules, cache, entry) {
 
   // Override the current require with this new one
   return newRequire;
-})({8:[function(require,module,exports) {
+})({7:[function(require,module,exports) {
 var bundleURL = null;
 function getBundleURLCached() {
   if (!bundleURL) {
@@ -100,7 +100,7 @@ function getBaseURL(url) {
 exports.getBundleURL = getBundleURLCached;
 exports.getBaseURL = getBaseURL;
 
-},{}],7:[function(require,module,exports) {
+},{}],6:[function(require,module,exports) {
 var bundle = require('./bundle-url');
 
 function updateLink(link) {
@@ -132,13 +132,13 @@ function reloadCSS() {
 
 module.exports = reloadCSS;
 
-},{"./bundle-url":8}],5:[function(require,module,exports) {
+},{"./bundle-url":7}],3:[function(require,module,exports) {
 
         var reloadCSS = require('_css_loader');
         module.hot.dispose(reloadCSS);
         module.hot.accept(reloadCSS);
       
-},{"_css_loader":7}],9:[function(require,module,exports) {
+},{"_css_loader":6}],8:[function(require,module,exports) {
 
 var global = (1, eval)('this');
 var OldModule = module.bundle.Module;
@@ -158,7 +158,7 @@ module.bundle.Module = Module;
 
 if (!module.bundle.parent && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
-  var ws = new WebSocket('ws://' + hostname + ':' + '55415' + '/');
+  var ws = new WebSocket('ws://' + hostname + ':' + '57322' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
@@ -259,5 +259,5 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.require, id);
   });
 }
-},{}]},{},[9])
+},{}]},{},[8])
 //# sourceMappingURL=/dist/fbb714a9d868a755d3f3ddba7c5cc70f.map
